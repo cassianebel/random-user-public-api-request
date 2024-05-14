8bd988b748d09a5ee174138fe0b35a15a276999e
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-05-13 22:05:20 -0500

    displays modal

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,17 @@
  * Public API Requests
  * Displays 12 'employees' using the Random User API
  */
-
+const gallery = document.getElementById('gallery');
+let employees = [];
 
 /**
  * Get 12 random people from the Random User API
  */
 async function getUsers() {
-  fetch("https://randomuser.me/api/?results=12&nat=us,au,gb")
-  .then(response => response.json())
-  .then(data => displayEmployees(data.results))
-  .catch(error => console.log(error))
+  const response = await fetch("https://randomuser.me/api/?results=12&nat=us,au,gb")
+  const data = await response.json();
+  employees = data.results;
+  displayEmployees(data.results);
 }
 
 
@@ -22,7 +23,7 @@ async function getUsers() {
  * Take an array of people objects and create the HTML
  * to display their info on the page
  * 
- * @param {array} data 
+ * @param {array} data - an array of people objects
  */
 function displayEmployees(data) {
   console.log(data);
@@ -39,9 +40,50 @@ function displayEmployees(data) {
                     </div>
                 </div>
                 `;
-    document.getElementById('gallery').insertAdjacentHTML('beforeend', html);
+    gallery.insertAdjacentHTML('beforeend', html);
   });
 }
 
-
 getUsers();
+
+gallery.addEventListener('click', (event) => {
+  const personCard = event.target.closest('.card');
+  if (!personCard) return;
+  const personName = personCard.querySelector('.card-name').textContent;
+  console.log(personName);
+  const person = employees.find(
+    (person) => `${person.name.first} ${person.name.last}` === personName
+  );
+  console.log(person);
+  displayModal(person);
+});
+
+
+function displayModal(person) {
+  let html = `
+    <div class="modal-container">
+      <div class="modal">
+          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+          <div class="modal-info-container">
+              <img class="modal-img" src="${person.picture.large}" alt="${person.name.first} ${person.name.last}'s profile picture">
+              <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+              <p class="modal-text">${person.email}</p>
+              <p class="modal-text cap">${person.location.city}</p>
+              <hr>
+              <p class="modal-text">${person.phone}</p>
+              <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
+              <p class="modal-text">Birthday: ${new Date(person.dob.date).toLocaleDateString(undefined, {month: 'numeric', day: 'numeric', year: 'numeric'})}</p>
+          </div>
+      </div>
+
+      // IMPORTANT: Below is only for exceeds tasks 
+      <div class="modal-btn-container">
+          <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+          <button type="button" id="modal-next" class="modal-next btn">Next</button>
+      </div>
+    </div>
+  `;
+  gallery.insertAdjacentHTML('afterend', html);
+}
+
+
